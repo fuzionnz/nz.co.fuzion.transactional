b9cb1777c1ca75c5d431a2f1ed05c63d1ab31083
Jitendra Purohit <jitendra@fuzion.co.nz>
2017-07-25 19:32:13 +0530

    Add postinstall hook

--- transactional.php
+++ transactional.php
@@ -121,6 +121,15 @@ function transactional_civicrm_install() {
 }
 
 /**
+ * Implements hook_civicrm_postInstall().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_postInstall
+ */
+function transactional_civicrm_postInstall() {
+  _transactional_civix_civicrm_postInstall();
+}
+
+/**
  * Implements hook_civicrm_uninstall().
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_uninstall
