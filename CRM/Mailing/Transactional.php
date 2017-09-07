1010b1dbf948fc65312d031ce02be63137c69ee9
Chris Burgess <chris@giantrobot.co.nz>
2017-09-07 15:25:07 +1200

    Bail out if no returnPath set.
    This was triggering an E_NOTICE when not set (test case was sending receipt from contribution page).
    
    Refs #12

--- CRM/Mailing/Transactional.php
+++ CRM/Mailing/Transactional.php
@@ -80,8 +80,11 @@ class CRM_Mailing_Transactional {
    * @return voic
    */
   public function delivered($params) {
-    $parts = explode(CRM_Core_Config::singleton()->verpSeparator, $params['returnPath']);
+    if (!isset($params['returnPath'])) {
+      return;
+    }
 
+    $parts = explode(CRM_Core_Config::singleton()->verpSeparator, $params['returnPath']);
     $delivered = new CRM_Mailing_Event_BAO_Delivered();
     $delivered->event_queue_id = $parts[2];
     $delivered->time_stamp = date('YmdHis');
