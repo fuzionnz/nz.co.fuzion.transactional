0485db3ad68934fcee86bedef2abdb2584e06cff
Eileen McNaughton <emcnaughton@wikimedia.org>
2023-09-05 13:13:48 +1200

    Use preferred, simpler, method to get settings

--- CRM/Mailing/MailingContactGetCountAPIWrapper.php
+++ CRM/Mailing/MailingContactGetCountAPIWrapper.php
@@ -10,11 +10,9 @@ class CRM_Mailing_MailingContactGetCountAPIWrapper implements API_Wrapper {
   * alter the result before returning it to the caller.
   */
   public function toApiOutput($apiRequest, $result) {
-    $transactionalMailings = civicrm_api3('Setting', 'get', [
-      'sequential' => 1,
-      'return' => ["transactional_mailings"],
-    ]);
-    if (empty($apiRequest['params']['contact_id']) || empty($transactionalMailings['values'][0]['transactional_mailings'])) {
+    $transactionalMailings = \Civi::settings()->get('transactional_mailings');
+
+    if (empty($apiRequest['params']['contact_id']) || empty($transactionalMailings)) {
       return $result;
     }
     $apiRequest['params']['options']['limit'] = 0;
